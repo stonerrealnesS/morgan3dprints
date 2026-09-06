3abdedc200bef56b16ab810df4a33160fd0df3ea
stonerrealnesS <stonerrealness@gmail.com>
2026-09-06 17:57:02 -0500

    Allow Whatnot CDN images for product photos (Whatnot sync)

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "items-images-production.s3.us-west-2.amazonaws.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.s3.amazonaws.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.s3.*.amazonaws.com",
-        pathname: "/**",
-      },
-    ],
-  },
+    images: {
+          remotePatterns: [
+            {
+                      protocol: "https",
+                      hostname: "res.cloudinary.com",
+                      pathname: "/**",
+            },
+            {
+                      protocol: "https",
+                      hostname: "items-images-production.s3.us-west-2.amazonaws.com",
+                      pathname: "/**",
+            },
+            {
+                      protocol: "https",
+                      hostname: "*.s3.amazonaws.com",
+                      pathname: "/**",
+            },
+            {
+                      protocol: "https",
+                      hostname: "*.s3.*.amazonaws.com",
+                      pathname: "/**",
+            },
+            {
+                      protocol: "https",
+                      hostname: "images.whatnot.com",
+                      pathname: "/**",
+            },
+                ],
+    },
 };
 
 export default nextConfig;
